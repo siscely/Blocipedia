78f54a9e2a7d33978ce1f5513688bbc4dc8cacdb
siscely <siscely.george@gmail.com>
2017-12-09 20:50:14 +0000

    changes to collaborator

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -3,6 +3,10 @@ class CollaboratorsController < ApplicationController
     @users = User.all
     @wiki = Wiki.find(params[:wiki_id])
     @wiki.collaborators.new(user_id: params[:user_id])
+    @wiki.collaborator_for(user)
+    if @wiki.collaborators.include?(user)
+      flash[:notice] = "Collaborator was saved."
+       redirect_to wiki_collaborator_path
+    end
   end
-
 end
